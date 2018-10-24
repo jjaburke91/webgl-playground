135265abaa58e13be7564864bc62372476d35c83
Jamie Burke <jjaburke@hotmail.com>
2018-10-24 17:14:40 +0100

    minortweaks to settings and colours.

--- js/app.js
+++ js/app.js
@@ -23,30 +23,76 @@ function createCamera() {
     return camera;
 }
 
+let renderPass, glitchPass, afterImagePass, bokehPass, unrealBloomPass;
 function addRenderPass() {
-    var renderPass = new THREE.RenderPass(scene, camera);
+    renderPass = new THREE.RenderPass(scene, camera);
     composer.addPass(renderPass);
     return renderPass;
 }
 
 function addGlitchPass() {
-    var glitchPass = new THREE.GlitchPass(0);
+    glitchPass = new THREE.GlitchPass(0);
     composer.addPass(glitchPass);
     return glitchPass;
 }
 
 function addAfterImagePass() {
-    var afterImagePass = new THREE.AfterimagePass(0.98);
+    afterImagePass = new THREE.AfterimagePass(0.98);
+
+    const MAX_DAMP = 1.01;
+    const MIN_DAMP = 0.92;
+    setInterval(() => {
+        afterImagePass.uniforms.damp.value = Math.random() * (MAX_DAMP - MIN_DAMP) + MIN_DAMP;
+    }, 1200);
+
     composer.addPass(afterImagePass);
     return afterImagePass;
 }
 
+function addBokehPass() {
+    bokehPass = new THREE.BokehPass(scene, camera, {
+        focus: 		0,
+        aperture:	0.1,
+        maxblur:	1,
+
+        width: width,
+        height: height
+    });
+
+    const MAX_FOCUS = 0.7;
+    const MIN_FOCUS = 0.1;
+    setInterval(() => {
+        bokehPass.uniforms.focus.value = Math.random() * (MAX_FOCUS - MIN_FOCUS) + MIN_FOCUS;
+    }, 900);
+
+    composer.addPass(bokehPass);
+    return bokehPass;
+}
+
+function addUnrealBloomPass() {
+    unrealBloomPass = new THREE.UnrealBloomPass(
+        new THREE.Vector2( window.innerWidth, window.innerHeight ),
+        1, // strength
+        0.1, // radius
+        0.8 // threshold
+    );
+
+    const MAX_STRENGTH = 50;
+    const MIN_STRENGTH = 1;
+    setInterval(() => {
+        unrealBloomPass.copyUniforms.opacity.value = Math.random() * (MAX_STRENGTH - MIN_STRENGTH) + MIN_STRENGTH;
+    }, 300);
+
+    composer.addPass(unrealBloomPass);
+    return unrealBloomPass;
+}
+
 /**
  * Post Processes to play with:
- * - Unreal / bloom
- * - dof / blur
- * - pixelation
- * - glitch DONE
+ * - Unreal / Bloom DONE
+ * - Dof / Blur DONE
+ * - Pixelation
+ * - Glitch DONE
  * - After image (motion blur) DONE
  */
 
@@ -61,31 +107,35 @@ renderer.setClearColor(COLORS.grey);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// createLight();
+createLight();
 const camera = createCamera();
 
 // Shapes
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshLambertMaterial({color: COLORS.green, transparent: false, opacity: 1});
 
+const material = new THREE.MeshLambertMaterial({color: COLORS.green, transparent: false, opacity: 1});
 const cube = new THREE.Mesh( geometry, material );
-const edges = new THREE.EdgesHelper(cube, COLORS.white );
+// scene.add( cube );
+
+const edges = new THREE.EdgesHelper(cube, COLORS.orange );
 edges.material.linewidth = 5;
 scene.add( edges );
-// scene.add( cube );
 
 /******/
 // Post Processing
 const DO_GLITCH_PASS = 0;
 const DO_AFTERIMAGE_PASS = 1;
+const DO_BLUR_PASS = 0;
+const DO_UNREALBLOOM_PASS = 1;
 
 const composer = new THREE.EffectComposer(renderer);
 
 let pass = addRenderPass();
 if (DO_GLITCH_PASS) pass = addGlitchPass();
 if (DO_AFTERIMAGE_PASS) pass = addAfterImagePass();
+if (DO_BLUR_PASS) pass = addBokehPass();
+if (DO_UNREALBLOOM_PASS) pass = addUnrealBloomPass();
 pass.renderToScreen = true;
-
 
 /******/
 // Animation
@@ -93,10 +143,6 @@ pass.renderToScreen = true;
 function animate(objectToAnimate) {
     objectToAnimate.rotation.x += 0.005;
     objectToAnimate.rotation.y += 0.005;
-
-    if (DO_AFTERIMAGE_PASS) {
-        afterimagePass.uniforms[ "damp" ], 'value', 0, 1);
-    }
 
     if (typeof composer !== 'undefined') {
         composer.render();
